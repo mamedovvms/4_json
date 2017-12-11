6c78f7c895dc30ac6071a3e762c77588f46996aa
mamedovvms <mamedovvs@mail.ru>
2017-12-11 11:02:34 +0700

    Добавлен код функций

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,15 @@
 import json
+import sys
 
 
 def load_data(filepath):
-    pass
-
+    with open(filepath) as f:
+        return json.loads(f.read())
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, indent=4))
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv[1]) > 1:
+        pretty_print_json(load_data(sys.argv[1]))
