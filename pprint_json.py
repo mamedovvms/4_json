5c7623c4f4203a69754ed1370df09629ac8bee47
mamedovvms <mamedovvs@mail.ru>
2017-12-11 11:15:07 +0700

    Добавлены необходимые описания для файла Изменено имя переменной

--- pprint_json.py
+++ pprint_json.py
@@ -6,8 +6,8 @@ def load_data(filepath):
     with open(filepath) as f:
         return json.loads(f.read())
 
-def pretty_print_json(data):
-    print(json.dumps(data, indent=4))
+def pretty_print_json(data_json):
+    print(json.dumps(data_json, indent=4))
 
 
 if __name__ == '__main__':
