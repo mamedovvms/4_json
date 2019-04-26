0a7ff38dad68ffe3ba045fda02236914b2145951
1237 <mamedov_vs@oksw.ru>
2019-04-26 11:32:25 +0700

    Добавлены проверки на существование файла. Добавлена проверка на содержимое файла в формате json. Поправлен вывод кириллицы.

--- pprint_json.py
+++ pprint_json.py
@@ -1,15 +1,31 @@
 import json
 import sys
+import os
 
 
 def load_data(filepath):
-    with open(filepath) as f:
-        return json.loads(f.read())
+    with open(filepath) as file:
+        try:
+            return json.loads(file.read())
+        except json.JSONDecodeError:
+            return
 
-def pretty_print_json(data_json):
-    print(json.dumps(data_json, indent=4))
+def pretty_print_json(data_for_print):
+    print(json.dumps(data_for_print, indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    if len(sys.argv[1]) > 1:
-        pretty_print_json(load_data(sys.argv[1]))
+    if len(sys.argv[1]) == 0:
+        print('Не введен путь к файлу')
+        exit(0)
+
+    filepath = sys.argv[1]
+    if not os.path.exists(filepath):
+        print('Файл не найден')
+        exit(0)
+    content_file_json = load_data(filepath)
+    if not content_file_json:
+        print('Содержимое файла не в формате json')
+        exit(0)
+
+    pretty_print_json(content_file_json)
